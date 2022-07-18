665b3b8a55ed1b1573eb2cd229afbab313a3062c
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2022-07-18 18:40:21 +0200

    Added jar packaging option

---
+++ console_unsafe.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar target/PasswordGenerator.jar pwgen.config.forceSafeConsole=false pwgen.config.copyPasswordToClipboard=true pwgen.config.echoPassword=false pwgen.generator.version=ALEPH
