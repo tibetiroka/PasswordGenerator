906c42b3882bd96adfd3a0561d4687e14a7b5852
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2022-07-20 09:19:45 +0200

    Allowed using higher existing security over the minimal recommendation

--- console_unsafe.sh
+++ console_unsafe.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar target/PasswordGenerator.jar pwgen.config.forceSafeConsole=false pwgen.config.copyPasswordToClipboard=true pwgen.config.echoPassword=false pwgen.generator.version=ALEPH
+java -jar target/PasswordGenerator.jar pwgen.config.hashSecurity=HighestExistingOrMedium pwgen.config.forceSafeConsole=false pwgen.config.copyPasswordToClipboard=true pwgen.config.echoPassword=false pwgen.generator.version=ALEPH
